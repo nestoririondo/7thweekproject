6a93ea0f9048ac4aa9c1071da3d4a84a810794e0
Moustafa Nady <mostafanadii93@gmail.com>
2024-01-24 20:33:36 +0100

    updated index.js

--- index.js
+++ index.js
@@ -1,30 +1,40 @@
-console.log("Hello project 7th!");
-
 const http = require("node:http");
 const PORT = 8000;
 
 //Create the server
 const server = http.createServer((req, res) => {
+    console.log("Hello project 7th!");
   if (req.url === "/") {
     res.statusCode = 200;
     res.setHeader("Content-Type", "application/json");
+
+
     const data = {
       message: "Hello from index",
+      timestamp: new Date().toISOString(),
     };
+
     const jsonData = JSON.stringify(data);
     res.end(jsonData);
   } else {
     res.statusCode = 200;
     res.setHeader("Content-Type", "application/json");
+
     const data = {
       message: "404 not found",
+      timestamp: new Date().toISOString(),
     };
     const jsonData = JSON.stringify(data);
     res.end(jsonData);
   }
+
 });
 
 //Start the server
 server.listen(PORT, () => {
   console.log(`Server listening port ${PORT}`);
+});
+
+server.on("error", (error) => {
+    console.error(error);
 });
